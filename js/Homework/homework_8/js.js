1bde729ff8798f827c37f4e93873893d96d02337
petrivym <petriv.dev@gmail.com>
2021-04-21 00:36:57 +0300

    js Homework 8 ready!

--- js/Homework/homework_8/js.js
+++ js/Homework/homework_8/js.js
@@ -56,7 +56,8 @@
 // -Дан текстареа. В него можно ввести данные, нажать кнопку "сохранить" и они "фикисруются" (в хранилище), затем поредактировать их, затем еще поредактировать и возможно еще.....
 // Требование : хранить историю своих изменений (даже после перезагрузки страницы).
 // Сверху над текстареа должны появится стрелочки, с помощью которых можно перемещаться по истории (не забудьте!чекпоинт истории - нажатеи кнопки сохранить).
-
+//
+//////////////////////////////////////////////////////////// Перша тренувальна версія
 // const textArea = document.getElementById('textArea');
 // const saveButton = document.getElementById('save');
 // const forwardButton = document.getElementById('forward');
@@ -98,19 +99,146 @@
 //     }
 // }
 
+// Другу удосконалена розявязок з відео
+// const textArea = document.getElementById('textArea');
+// const saveButton = document.getElementById('save');
+// const forwardButton = document.getElementById('forward');
+// const backButton = document.getElementById('back');
+// const INPUT = 'INPUT';
+// let counter = 0;
+//
+// if (!localStorage.getItem(INPUT)) {
+//     localStorage.setItem(INPUT, '[]');
+// }else {
+//     const db = JSON.parse(localStorage.getItem(INPUT));
+//     counter = db.length-1
+//     textArea.value = db[counter];
+// }
+//
+// saveButton.onclick = ({target}) => {
+//     const data = textArea.value;
+//     const db = JSON.parse(localStorage.getItem(INPUT))
+//     db.push(data)
+//     localStorage.setItem(INPUT, JSON.stringify(db))
+//     counter++
+// }
+//
+// backButton.onclick = () => {
+//     const db = JSON.parse(localStorage.getItem(INPUT))
+//     if (counter - 1 >= 0) {
+//       counter--
+//     }else {
+//         counter = db.length - 1;
+//     }
+//       textArea.value = db[counter];
+// }
+//
+// forwardButton.onclick = () => {
+//     const db = JSON.parse(localStorage.getItem(INPUT))
+//     if (counter + 1 < db.length) {
+//     counter++
+//     }else {
+//         counter = 0;
+//     }
+//     textArea.value = db[counter];
+// }
 
 // - Реализуйте записную книгу, хранящую данные в локальном хранилище.
 //     Данные которые надо сохранять : ФИО, номер, почта, фирма, отдел, день рождения
 // Данные вводить через соответсвующую форму.
 // --Каждому контакту добавить кнопку для удаления контакта.
 // --Каждому контакту добавить кнопку редактироваиня. При нажати на нее появляется форма, в которой есть все необходимые инпуты для редактирования, которые уже заполнены данными объекта
-// navigator.geolocation.getCurrentPosition((data)=>{
-//     console.log(data);
-// });
 
 
+const form = document.getElementById('form-info');
+const saveBtn = document.getElementById('save');
+const backBtn = document.getElementById('back');
+const forwardBtn = document.getElementById('forward');
+let deleteBTN = document.getElementById('delete');
+let remove = document.getElementById('remove');
+
+const PERSON = 'person'
+let objInform = {};
+let counter = -1;
 
 
+if (!localStorage.getItem(PERSON)) {
+    localStorage.setItem(PERSON, '[]');
+} else {
+    console.log('reload...');
+}
+
+form.oninput = ({target: {value, name}}) => {
+    objInform[name] = value;
+}
+
+saveBtn.onclick = () => {
+    let getPersonArray = JSON.parse(localStorage.getItem(PERSON));
+    getPersonArray.push(objInform);
+    localStorage.setItem(PERSON, JSON.stringify(getPersonArray));
+    counter++
+}
+
+backBtn.onclick = () => {
+    let getPersonArray = JSON.parse(localStorage.getItem(PERSON));
+    counter--;
+    if (!(counter >= 0)) {
+        counter = getPersonArray.length - 1
+    }
+    for (const formInput of form) {
+        for (const formInputKey in getPersonArray[counter]) {
+            if (formInput.name === formInputKey) {
+                formInput.value = getPersonArray[counter][formInputKey];
+            }
+        }
+    }
+}
+
+forwardBtn.onclick = () => {
+    let getPersonArray = JSON.parse(localStorage.getItem(PERSON));
+    counter++;
+    if (!(counter <= getPersonArray.length - 1)) {
+        counter = 0;
+    }
+
+    for (const formInput of form) {
+        for (const formInputKey in getPersonArray[counter]) {
+            if (formInput.name === formInputKey) {
+                formInput.value = getPersonArray[counter][formInputKey];
+            }
+        }
+    }
+}
+
+deleteBTN.onclick = () => {
+    let getPersonArray = JSON.parse(localStorage.getItem(PERSON));
+    getPersonArray.splice(counter, 1);
+    localStorage.setItem(PERSON, JSON.stringify(getPersonArray));
+
+
+    if (getPersonArray.length === 0) {
+        for (const formItem of form) {
+            formItem.value = '';
+        }
+    }
+
+    for (const formInput of form) {
+        for (const formInputKey in getPersonArray[counter]) {
+            if (formInput.name === formInputKey) {
+                formInput.value = getPersonArray[counter][formInputKey];
+            }
+        }
+    }
+}
+remove.onclick = () => {
+    let getPersonArray = JSON.parse(localStorage.getItem(PERSON));
+    let removeObj = {};
+    for (const formValue of form) {
+        removeObj[formValue.name] = formValue.value;
+    }
+    getPersonArray[counter] =  removeObj;
+    localStorage.setItem(PERSON,JSON.stringify(getPersonArray));
+}
 
 
 
